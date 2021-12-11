7a57604cf7ec48caf7a57028e0813e6cf05fa9cb
cre4tive97 <harryborrison5148@gmail.com>
2021-12-11 21:17:16 +0900

    App.vue TS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <AppHeader />
+    <router-view></router-view>
   </div>
-  <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppHeader from "@/components/common/AppHeader.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AppHeader,
+  },
+});
+</script>
+
 <style>
+@import "./css/reset.css";
+@import "./css/style.css";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  height: 100vh;
 }
 </style>
