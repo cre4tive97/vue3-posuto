cafae81264f26a4717d1d109c2f3c2d1b73890bb
cre4tive97 <harryborrison5148@gmail.com>
2021-12-11 21:16:11 +0900

    SignupPage.vue TS

--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -8,9 +8,11 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { defineComponent } from "vue";
 import SignupForm from "@/components/SignupForm.vue";
-export default Vue.extend({
+
+export default defineComponent({
+  name: "SignupPage",
   components: {
     SignupForm,
   },
