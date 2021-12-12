db8fbd52fc72195979c0bd45e61401aeef80553d
cre4tive97 <harryborrison5148@gmail.com>
2021-12-12 13:22:54 +0900

    axios interceptors type

--- src/api/common/interceptors.ts
+++ src/api/common/interceptors.ts
@@ -1,17 +1,20 @@
-import store from "@/store/index";
-import { AxiosInstance } from "axios";
+import { useStore } from "@/store/index";
+import {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosError,
+  AxiosResponse,
+} from "axios";
 
-export function setInterceptors(instance: AxiosInstance) {
-  // Add a request interceptor
+const store = useStore();
+
+export function setInterceptors(instance: AxiosInstance): AxiosInstance {
   instance.interceptors.request.use(
     function (config) {
-      // Do something before request is sent
-      // console.log(config);
-      config.headers.Authorization = store.state.token;
+      if (config?.headers) config.headers.Authorization = store.state.token;
       return config;
     },
-    function (error) {
-      // Do something with request error
+    function (error: AxiosError) {
       return Promise.reject(error);
     }
   );
@@ -19,13 +22,9 @@ export function setInterceptors(instance: AxiosInstance) {
   // Add a response interceptor
   instance.interceptors.response.use(
     function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
       return response;
     },
     function (error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
       return Promise.reject(error);
     }
   );
