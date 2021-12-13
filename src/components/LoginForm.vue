3763c596e084a026351abf7e6aee801af06f3f85
cre4tive97 <harryborrison5148@gmail.com>
2021-12-13 18:01:32 +0900

    LoginForm.vue script setup

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,3 +1,45 @@
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import { ActionTypes } from "@/store/actions";
+import { validateUsername } from "@/utils/validation";
+import { useStore } from "@/store/index";
+import { useRouter } from "vue-router";
+
+// store, router
+const store = useStore();
+const router = useRouter();
+
+// data
+const username = ref("");
+const password = ref("");
+
+// computed
+const isUsernameValid = computed(() => validateUsername(username.value));
+
+// methods
+// 폼 제출
+async function submitForm() {
+  try {
+    await store.dispatch(ActionTypes.LOGIN_USER, {
+      username: username.value,
+      password: password.value,
+    });
+    router.push("/main");
+  } catch (error: any) {
+    if (error.response.status === 401) {
+      alert("Username 또는 Password가 틀렸습니다!");
+    }
+  } finally {
+    initForm();
+  }
+}
+
+function initForm() {
+  username.value = "";
+  password.value = "";
+}
+</script>
+
 <template>
   <form class="form" @submit.prevent="submitForm">
     <div class="form-box">
@@ -31,58 +73,3 @@
     </button>
   </form>
 </template>
-
-<script lang="ts">
-import { defineComponent, computed, ref } from "vue";
-import { ActionTypes } from "@/store/actions";
-import { validateUsername } from "@/utils/validation";
-import { useStore } from "@/store/index";
-import { useRouter } from "vue-router";
-
-export default defineComponent({
-  name: "LoginForm",
-
-  setup() {
-    // store, router
-    const store = useStore();
-    const router = useRouter();
-
-    // data
-    const username = ref("");
-    const password = ref("");
-
-    // computed
-    const isUsernameValid = computed(() => validateUsername(username.value));
-
-    // methods
-    // 폼 제출
-    async function submitForm() {
-      try {
-        await store.dispatch(ActionTypes.LOGIN_USER, {
-          username: username.value,
-          password: password.value,
-        });
-        router.push("/main");
-      } catch (error: any) {
-        if (error.response.status === 401) {
-          alert("Username 또는 Password가 틀렸습니다!");
-        }
-      } finally {
-        initForm();
-      }
-    }
-
-    function initForm() {
-      username.value = "";
-      password.value = "";
-    }
-
-    return {
-      isUsernameValid,
-      submitForm,
-      username,
-      password,
-    };
-  },
-});
-</script>
